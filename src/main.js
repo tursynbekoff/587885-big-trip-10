70d0afc39f7b4b05d662de4c4ff70584e80df416
Maria <itimasha@gmail.com>
2019-11-28 15:30:02 +0300

    missed semicolon

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const createRouteInfoTemplate = () => {
     <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
 
     <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
-  </div>`
+  </div>`;
 };
 
 const createSiteMenuTemplate = () => {
