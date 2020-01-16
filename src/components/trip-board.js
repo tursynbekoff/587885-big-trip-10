f2e4c695bda9c9a31565b1c60df6fa264b7297e8
Maria <itimasha@gmail.com>
2020-01-16 20:30:02 +0300

    rewrite board as class

--- src/components/trip-board.js
+++ src/components/trip-board.js
@@ -1,4 +1,27 @@
-export const createBoardTripTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createBoardTripTemplate = () => {
   return `<ul class="trip-days">
   </ul>`;
 };
+
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBoardTripTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
