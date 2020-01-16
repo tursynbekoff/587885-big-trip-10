4a40468608b29c2d86a33a70b4227966e6306294
Maria <itimasha@gmail.com>
2020-01-16 20:09:02 +0300

    rewrite sorting as class

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,4 +1,6 @@
-export const createSortingTemplate = () => {
+import {createElement} from '../utils.js';
+
+const createSortingTemplate = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
             <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
@@ -30,3 +32,24 @@ export const createSortingTemplate = () => {
             <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
           </form>`;
 };
+
+export default class Sorting {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSortingTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
